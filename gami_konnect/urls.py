2d575389f4e2af8b7cd1f8ec4af0b2ad0de9ea78
ValerieMbaka <valeriembaka0@gmail.com>
2026-04-17 00:12:30 +0300

    Updated email configuration

--- gami_konnect/urls.py
+++ gami_konnect/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('games/', include('games.urls')),
     path('accounts/', include('accounts.urls')),
     path('shops/', include('shops.urls')),
-    path('competitions/', include('competitions.urls')),
+    # path('competitions/', include('competitions.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
